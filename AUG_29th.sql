c8331c3f9a0a3de623ab1105335bde66a0a51fca
rohanmesta2000 <rohanmesta.xworkz@gmail.com>
2023-09-01 00:30:40 +0530

    push

---
+++ AUG_29th.sql
@@ -0,0 +1,23 @@
+SHOW databases;
+CREATE DATABASE jun_19th;
+CREATE DATABASE 5_MONTHS;
+CREATE DATABASE jun_20th;
+use jun_19th;
+use 5_MONTHS; 
+use jun_20th;
+
+
+use jun_19th;
+CREATE TABLE table_name(a_no int,name varchar(80),name_father varchar(20),pan varchar(67));
+SELECT * FROM table_name;
+DROP TABLE table_name;
+
+CREATE TABLE table_name(a_no int,name varchar(80),name_father varchar(20),pan varchar(67),mother_num varchar(30),brother varchar(80),brother_name char(rohit),sister_name char(pooja),phone_num int(86));
+
+
+                                                            ;
+
+
+
+
+
